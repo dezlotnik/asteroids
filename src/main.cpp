5a4565f391e8b421182a330d1b8801fb33758a4b
David Zlotnik <dzlotnik@umich.edu>
2020-10-28 17:16:51 -0700

    add classes from snake game

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include "renderer.h"
+#include "controller.h"
+#include "game.h"
 
 int main() {
-    std::cout << "Hello World!" << "\n";
-
     constexpr std::size_t kFramesPerSecond{60};
     constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
     constexpr std::size_t kScreenWidth{640};
@@ -12,40 +12,12 @@ int main() {
     constexpr std::size_t kGridHeight{32};
 
     Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
-
-    std::size_t target_frame_duration = kMsPerFrame;
-    Uint32 title_timestamp = SDL_GetTicks();
-    Uint32 frame_start;
-    Uint32 frame_end;
-    Uint32 frame_duration;
-    int frame_count = 0;
-    bool running = true;
-    while (running) {
-        frame_start = SDL_GetTicks();
-
-        renderer.Render();
-
-        frame_end = SDL_GetTicks();
-
-        // Keep track of how long each loop through the input/update/render cycle
-        // takes.
-        frame_count++;
-        frame_duration = frame_end - frame_start;
-
-        // After every second, update the window title.
-        if (frame_end - title_timestamp >= 1000) {
-        renderer.UpdateWindowTitle(0, frame_count);
-        frame_count = 0;
-        title_timestamp = frame_end;
-        }
-
-        // If the time for this frame is too small (i.e. frame_duration is
-        // smaller than the target ms_per_frame), delay the loop to
-        // achieve the correct frame rate.
-        if (frame_duration < target_frame_duration) {
-        SDL_Delay(target_frame_duration - frame_duration);
-        }
-    }
+    Controller controller;
+    Game game(kGridWidth, kGridHeight);
+    game.Run(controller, renderer, kMsPerFrame);
+    std::cout << "Game has terminated successfully!\n";
+    std::cout << "Score: " << game.GetScore() << "\n";
+    std::cout << "Size: " << game.GetSize() << "\n";
 
     return 0;
 }
