9de811e8654d9fe1d89663b9925e0b1721c88b0e
David Zlotnik <dzlotnik@1833-laptop.corp.zooxlabs.com>
2020-10-30 13:24:42 -0700

    update random generation

--- src/asteroid.cpp
+++ src/asteroid.cpp
@@ -1,14 +1,33 @@
 #include "asteroid.h"
+#include <iostream>
 
 Asteroid::Asteroid() :
     engine(dev()),
-    random_x(0, static_cast<float>(640)),
-    random_y(0, static_cast<float>(640)),
     random_heading(0, static_cast<float>(360)),
-    random_speed(2, static_cast<float>(5))
+    random_speed(2, static_cast<float>(5)),
+    random_angular_velocity(0.5,5.0),
+    random_position(0, static_cast<int>(2560)) //640 * 4
 {
-    setVelocity(random_speed(engine),random_heading(engine),3);
-    setPose(random_x(engine),random_y(engine),0);
+    int p = random_position(engine);
+    float y;
+    float x;
+
+    if (p >=0 && p <= 640) {
+        y = 0.0;
+        x = static_cast<float>( p % 640 );
+    } else if (p > 640 && p <=640*2) {
+        x = 640.0;
+        y = static_cast<float>( p % 640 );
+    } else if (p > 640*2 && p <=640*3) {
+        y = 640.0;
+        x = static_cast<float>( p % 640 );
+    } else {
+        y = static_cast<float>( p % 640 );
+        x = 0.0;
+    }
+
+    setVelocity(random_speed(engine),random_heading(engine),random_angular_velocity(engine));
+    setPose(x,y,0);
     file_name = "../data/meteorBrown_big1.png";
     image_width = 101;
     image_height = 85;
--- src/asteroid.h
+++ src/asteroid.h
@@ -11,10 +11,10 @@ public:
 private:
   std::random_device dev;
   std::mt19937 engine;
-  std::uniform_real_distribution<float> random_x;
-  std::uniform_real_distribution<float> random_y;
   std::uniform_real_distribution<float> random_speed;
   std::uniform_real_distribution<float> random_heading;
+  std::uniform_real_distribution<float> random_angular_velocity;
+  std::uniform_int_distribution<int> random_position;
 };
 
 #endif
