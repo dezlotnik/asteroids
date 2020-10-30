5b04edc6c21ceeff28e10c155fb7846eaff65691
David Zlotnik <dzlotnik@1833-laptop.corp.zooxlabs.com>
2020-10-29 21:03:40 -0700

    fix collision detection

--- src/collision_detection.cpp
+++ src/collision_detection.cpp
@@ -2,12 +2,14 @@
 #include "game_object.h"
 
 bool CollisionDetection::detect_collision(const GameObject &game_object_1, const GameObject &game_object_2) {
-    if (game_object_1.getPose().x >= game_object_2.getPose().x + game_object_2.image_width || 
-        game_object_2.getPose().x >= game_object_1.getPose().x + game_object_1.image_width ) {
+    // check if image boxes over lap
+    // https://www.geeksforgeeks.org/find-two-rectangles-overlap/
+    if (game_object_1.getPose().x - game_object_1.image_width/2 >= game_object_2.getPose().x + game_object_2.image_width/2 ||
+        game_object_2.getPose().x - game_object_2.image_width/2 >= game_object_1.getPose().x + game_object_1.image_width/2 ) {
         return false;
     }
-    if (game_object_1.getPose().y <= game_object_2.getPose().y - game_object_2.image_height || 
-        game_object_2.getPose().y <= game_object_1.getPose().y - game_object_1.image_height ) {
+    if (game_object_1.getPose().y + game_object_1.image_height/2 <= game_object_2.getPose().y - game_object_2.image_height/2 ||
+        game_object_2.getPose().y + game_object_1.image_height/2 <= game_object_1.getPose().y - game_object_1.image_height/2 ) {
         return false;
     }
     return true;
